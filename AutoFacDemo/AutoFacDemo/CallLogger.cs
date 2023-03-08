dcdabd9da00e6dc216ff515f8bcb17998a314aac
Jo Wagner <jowagner@limago.de>
2023-03-08 12:05:36 +0100

    initial commit

---
+++ AutoFacDemo/AutoFacDemo/CallLogger.cs
@@ -0,0 +1,51 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacDemo
+{
+    public class CallLogger : IInterceptor
+    {
+        TextWriter _output;
+
+        public CallLogger(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            _output.Write("Calling method {0} with parameters {1}... ",
+              invocation.Method.Name,
+              string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+            OnEntry();
+           try
+            {
+                invocation.Proceed(); // eigentlicher Methodenaufruf
+            } catch(Exception)
+            {
+                OnTrow();
+            }
+
+            OnExit();
+            _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+        }
+
+        private void OnEntry()
+        {
+
+        }
+
+        private void OnExit()
+        {
+
+        }
+        private void OnTrow()
+        {
+
+        }
+    }
+}
---
+++ AutoFacDemo/AutoFacDemo/Starter.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacDemo
+{
+    public class Starter
+    {
+        private static IContainer Container { get; set; }
+        public void run()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>().EnableInterfaceInterceptors();
+            //builder.Register(c => new CallLogger(Console.Out));
+            builder.Register(c => new CallLogger(Console.Out)).Named<IInterceptor>("log-calls");
+
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+
+        }
+       
+
+    }
+}
